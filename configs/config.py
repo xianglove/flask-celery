83af54755035ceda8872a85b12098252c4f015a4
夏翔 <xiaxiang@imdada.cn>
2019-01-03 18:28:06 +0800

    add

---
+++ configs/config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = 'mysql://dev_w:6nvjq0_HW@192.168.1.250:3307/dada'
+#     SQLALCHEMY_ECHO = True
+#     SQLALCHEMY_ENCODING = 'utf-8'
--- core/__init__.py
+++ core/__init__.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-app = Flask(__name__)
+# from configs.configs import Config
+# from flask_sqlalchemy import SQLAlchemy
+
+
+def create_app():
+    app = Flask(__name__)
+    # app.configs.from_object(Config)  配置读取
+    # db.init_app(app)  # 初始化 SQLAlchemy 链接池
+    return app
+
+
+app = create_app()
+# db = SQLAlchemy()
